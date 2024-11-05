be3997361693f85b64ebd3986426794154a7a190
danrb9 <danrbt79@gmail.com>
2024-11-05 17:38:05 +0300

    gh-pages manual build

--- src/types/routes.ts
+++
@@ -1,9 +0,0 @@
-export interface RouteConfig {
-  path: string;
-  content: React.ComponentType;
-  sidebar?: {
-    left?: React.ComponentType | null;
-    right?: React.ComponentType | null;
-    defaultOpen?: boolean;
-  };
-} 
